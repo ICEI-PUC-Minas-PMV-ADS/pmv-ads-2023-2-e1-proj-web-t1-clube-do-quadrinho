71c1b055b22aaf40cd5eec158c8d6b040e5cbbb2
Thiago Bastani <thiagobastani.tb@gmail.com>
2023-12-06 15:09:34 -0300

    alterar links

--- docs/paginas/paginaListas/script.js
+++ docs/paginas/paginaListas/script.js
@@ -33,7 +33,7 @@ function carregarLista() {
         aTitulo.href = `../lista/lista.html?id=${r.id}`;
         aTitulo.addEventListener('click', (event) => {
             event.preventDefault();
-            window.location.replace(`${window.location.origin}/codigo-fonte/src/paginas/paginaListas/lista/lista.html?id=${r.id}`);
+            window.location.replace(`${window.location.origin} paginas/paginaListas/lista/lista.html?id=${r.id}`);
         })
         aTitulo.innerHTML = r.titulo;
         tdTitulo.appendChild(aTitulo);
@@ -52,7 +52,7 @@ function carregarLista() {
         let aEditar = document.createElement('a');
         aEditar.addEventListener('click', function (event) {
             event.preventDefault();
-            window.location.replace(`${window.location.origin}/codigo-fonte/src/paginas/paginaListas/editarLista/editar-lista.html?id=${r.id}`);
+            window.location.replace(`${window.location.origin} paginas/paginaListas/editarLista/editar-lista.html?id=${r.id}`);
         }); //adiciona o evento de clique
         aEditar.innerHTML = '<i class="bi bi-pencil-fill"></i>';
         tdEditar.appendChild(aEditar);
