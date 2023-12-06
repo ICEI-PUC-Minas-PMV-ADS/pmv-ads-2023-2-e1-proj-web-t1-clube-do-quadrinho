606c6eb84c302e5857caaf912b34352ef9cc0037
Thiago Bastani <thiagobastani.tb@gmail.com>
2023-12-06 15:43:46 -0300

    criar env.baseUrl

--- docs/assets/util/logout.js
+++ docs/assets/util/logout.js
@@ -1,6 +1,7 @@
 import { logout } from "../../services/account-service/account-service.js"
+import { env } from "../../env.js";
 
 document.getElementById("sair").addEventListener("click", () => {
     logout();
-    window.location.replace(`${window.location.origin}/pmv-ads-2023-2-e1-proj-web-t1-clube-do-quadrinho/paginas/login/login-page.html`);
+    window.location.replace(`${env.baseUrl}/paginas/login/login-page.html`);
 });
